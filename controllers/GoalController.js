1a739a4bbb52aacee00a2cd0b8be8e6d5fff11f0
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2023-11-15 17:09:39 +0500

    express-async-handler added

--- controllers/GoalController.js
+++ controllers/GoalController.js
@@ -1,21 +1,36 @@
-const getGoals = (request, response) => {
-    response .status(200) .json({'message' : 'Get Goals'});
-};
+const asyncHandler = require('express-async-handler');
+
+const getGoals = asyncHandler( 
+    async (request, response) => {
+        response.status(200) .json({'message' : 'Get Goals'});
+    }
+);
 
 
-const createGoals = (request, response) => {
-    response.status(201).json({'message' : 'Create Goals'});
-};
+const createGoals = asyncHandler(
+    async (request, response) => {
+
+        if ( !request.body.text ) {
+            // response.status(400);
+            throw new Error('Text field is required 2');
+        }
+        response.status(201).json({'message' : 'Create Goals'});
+    }
+);
 
 
-const updateGoals = (request, response) => {
-    response .status(200) .json({'message' : `Update Goals ${request.params.id}`});
-};
+const updateGoals = asyncHandler(
+    async (request, response) => {
+        response.status(200) .json({'message' : `Update Goals ${request.params.id}`});
+    }
+);
 
 
-const deleteGoals = (request, response) => {
-    response .status(200) .json({'message' : `Delete Goals ${request.params.id}`});
-};
+const deleteGoals = asyncHandler(
+    async (request, response) => {
+        response.status(200) .json({'message' : `Delete Goals ${request.params.id}`});
+    }
+);
 
 
 
