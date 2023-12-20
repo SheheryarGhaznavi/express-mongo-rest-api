eb499edb498dd67cdd0676d22d1e8ac0d2ad4e6e
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2023-12-20 15:05:50 +0500

    auth middleware applied to users get route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, get } = require('../controllers/UserController');
+const protect = require('../middlewares/AuthMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/get', get);
+router.get('/get', protect, get);
 
 module.exports = router;
